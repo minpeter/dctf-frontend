a4004c38bc36a26bb8e4ef3d98f3bd8e1c994649
Woonggi Min <minpeter@friendli.ai>
2024-01-02 14:02:40 +0900

    Add new icons and update profile page layout

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -7,41 +7,87 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
-import { EnvelopeOpenIcon, Link2Icon } from "@radix-ui/react-icons";
+import {
+  EnvelopeOpenIcon,
+  Link2Icon,
+  AvatarIcon,
+  RocketIcon,
+  BackpackIcon,
+} from "@radix-ui/react-icons";
 
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { DataTable, DataTableDemo } from "./solveTable";
 export default function Page() {
   return (
-    <Card>
-      <CardHeader>
-        <CardTitle>username</CardTitle>
-        <CardDescription>
-          Please write any comments you would like to add
-        </CardDescription>
-      </CardHeader>
-      <CardContent>
-        <div className="grid gap-4 py-4">
-          <div className="flex items-center gap-1">
-            <Label htmlFor="email">
-              <EnvelopeOpenIcon className="h-5 w-5 mr-2" />
-            </Label>
-            <Input id="email" value="test@gmail.com" className="col-span-3" />
+    <>
+      <Card>
+        <CardHeader>
+          <CardTitle>minpeter</CardTitle>
+          <CardDescription>
+            This will change the way your name appears on the scoreboard. You
+            can change it once every 10 minutes.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div className="grid gap-4 py-4">
+            <div className="flex items-center gap-1">
+              <RocketIcon className="h-5 w-5 mr-2" /> No points earned
+            </div>
+            <div className="flex items-center gap-1">
+              <BackpackIcon className="h-5 w-5 mr-2" /> Unranked
+            </div>
           </div>
-          <div className="flex items-center gap-1">
-            <Label htmlFor="link">
-              <Link2Icon className="h-5 w-5 mr-2" />
-            </Label>
-            <Input id="link" value="https://test.com" className="col-span-3" />
+        </CardContent>
+      </Card>
+      <Card>
+        <CardHeader>
+          <CardTitle>Update Information</CardTitle>
+          <CardDescription>
+            This will change the way your name appears on the scoreboard. You
+            can change it once every 10 minutes.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div className="grid gap-4 py-4">
+            <div className="flex items-center gap-1">
+              <Label htmlFor="name">
+                <AvatarIcon className="h-5 w-5 mr-2" />
+              </Label>
+              <Input id="name" value="username" className="col-span-3" />
+            </div>
+            <div className="flex items-center gap-1">
+              <Label htmlFor="email">
+                <EnvelopeOpenIcon className="h-5 w-5 mr-2" />
+              </Label>
+              <Input id="email" value="test@gmail.com" className="col-span-3" />
+            </div>
+            <div className="flex items-center gap-1">
+              <Label htmlFor="link">
+                <Link2Icon className="h-5 w-5 mr-2" />
+              </Label>
+              <Input
+                id="link"
+                value="https://test.com"
+                className="col-span-3"
+              />
+            </div>
           </div>
-        </div>
-      </CardContent>
-      <CardFooter>
-        {/* update button */}
-        <Button className="w-full">Update</Button>
+        </CardContent>
+        <CardFooter>
+          <Button className="w-full">Update</Button>
+        </CardFooter>
+      </Card>
 
-        {/* delete button */}
-      </CardFooter>
-    </Card>
+      {/* sovles */}
+      <Card>
+        <CardHeader>
+          <CardTitle>Solves</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <DataTableDemo />
+        </CardContent>
+      </Card>
+    </>
   );
 }
