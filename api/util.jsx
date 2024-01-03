cb35a046de1df2aee66fa02c52419ed27558af62
minpeter <kail2005611@gmail.com>
2024-01-03 09:13:22 +0900

    Add client import and update UI components

--- api/util.jsx
+++ api/util.jsx
@@ -1,3 +1,5 @@
+"use client";
+
 export function relog() {
   localStorage.removeItem("token");
   window.location.href = "/login";
--- app/challs/page.tsx
+++ app/challs/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   CardTitle,
   CardHeader,
@@ -21,9 +23,59 @@ import { Label } from "@/components/ui/label";
 
 import Link from "next/link";
 
+import Fire from "@/components/confetti";
+
 export default function Page() {
   return (
     <>
+      <Card>
+        <CardHeader>
+          <CardTitle className="text-lg font-bold flex justify-between space-x-20">
+            <div className="flex space-x-4">
+              <h3>misc/sanity-check</h3>
+              <div className="flex space-x-2">
+                <Badge variant="outline">dynamic</Badge>
+                <Badge variant="outline">Running</Badge>
+              </div>
+            </div>
+
+            <p>1 solves / 485 points</p>
+          </CardTitle>
+          <CardDescription>minpeter</CardDescription>
+        </CardHeader>
+        <CardContent className="space-y-4 py-4">
+          <p>I get to write the sanity check challenge! Alright!</p>
+
+          <div className="flex w-full items-center space-x-2">
+            <Input type="flag" placeholder="Flag" />
+            <Button type="submit" onClick={Fire}>
+              Submit
+            </Button>
+          </div>
+        </CardContent>
+        <CardFooter className="flex justify-between items-center bg-gray-100 py-4 dark:bg-gray-800">
+          <div className="space-y-2">
+            <Label htmlFor="downloads">Downloads</Label>
+            <div className="flex space-x-2">
+              <Link
+                href="#none"
+                className={badgeVariants({ variant: "outline" })}
+              >
+                ciphertext.txt
+              </Link>
+              <Link
+                href="#none"
+                className={badgeVariants({ variant: "outline" })}
+              >
+                substitute.py
+              </Link>
+            </div>
+          </div>
+
+          <Button>Instance Start</Button>
+        </CardFooter>
+      </Card>
+
       <Card>
         <CardHeader>
           <CardTitle className="text-lg font-bold flex justify-between space-x-20">
@@ -93,7 +145,9 @@ export default function Page() {
             <TabsContent value="flag">
               <div className="flex w-full items-center space-x-2">
                 <Input type="flag" placeholder="Flag" />
-                <Button type="submit">Sumit</Button>
+                <Button type="submit" onClick={Fire}>
+                  Sumit
+                </Button>
               </div>
             </TabsContent>
           </Tabs>
@@ -157,7 +211,9 @@ export default function Page() {
 
           <div className="flex w-full items-center space-x-2">
             <Input type="flag" placeholder="Flag" />
-            <Button type="submit">Sumit</Button>
+            <Button type="submit" onClick={Fire}>
+              Sumit
+            </Button>
           </div>
         </CardContent>
         <CardFooter className="flex justify-between items-center bg-gray-100 py-4 dark:bg-gray-800">
@@ -189,52 +245,6 @@ export default function Page() {
               </TooltipContent>
             </Tooltip>
           </TooltipProvider>
-        </CardFooter>
-      </Card>
-
-      <Card>
-        <CardHeader>
-          <CardTitle className="text-lg font-bold flex justify-between space-x-20">
-            <div className="flex space-x-4">
-              <h3>misc/sanity-check</h3>
-              <div className="flex space-x-2">
-                <Badge variant="outline">dynamic</Badge>
-                <Badge variant="outline">Running</Badge>
-              </div>
-            </div>
-
-            <p>1 solves / 485 points</p>
-          </CardTitle>
-          <CardDescription>minpeter</CardDescription>
-        </CardHeader>
-        <CardContent className="space-y-4 py-4">
-          <p>I get to write the sanity check challenge! Alright!</p>
-
-          <div className="flex w-full items-center space-x-2">
-            <Input type="flag" placeholder="Flag" />
-            <Button type="submit">Subscribe</Button>
-          </div>
-        </CardContent>
-        <CardFooter className="flex justify-between items-center bg-gray-100 py-4 dark:bg-gray-800">
-          <div className="space-y-2">
-            <Label htmlFor="downloads">Downloads</Label>
-            <div className="flex space-x-2">
-              <Link
-                href="#none"
-                className={badgeVariants({ variant: "outline" })}
-              >
-                ciphertext.txt
-              </Link>
-              <Link
-                href="#none"
-                className={badgeVariants({ variant: "outline" })}
-              >
-                substitute.py
-              </Link>
-            </div>
-          </div>
-
-          <Button>Instance Start</Button>
         </CardFooter>
       </Card>
     </>
